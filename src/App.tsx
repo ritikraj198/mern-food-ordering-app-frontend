43c18b5f96c779ab80ca55ee00f4f8d2170d95cb
Ritik <ritikraj198@gmail.com>
2025-07-26 17:00:30 +0530

    Removed unused variables from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button } from "./components/ui/button";
+// // import { useState } from "react";
+// // import reactLogo from "./assets/react.svg";
+// // import viteLogo from "/vite.svg";
+// import "./App.css";
+// import { Button } from "./components/ui/button";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   // const [count, setCount] = useState(0);
 
-  return <Button>Click me</Button>;
-}
+//   return <Button>Click me</Button>;
+// }
 
-export default App;
+// export default App;
