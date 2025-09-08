f03cef67d95a773029a214f7a583cf2e0672f5f4
Ritik <ritikraj198@gmail.com>
2025-09-08 12:24:37 +0530

    adding view order features

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,6 +7,7 @@ import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 import SearchPage from "./pages/SearchPage";
 import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
 
 const AppRoutes = () => {
   return (
@@ -37,6 +38,16 @@ const AppRoutes = () => {
           </Layout>
         }
       />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/order-status"
+          element={
+            <Layout>
+              <OrderStatusPage />
+            </Layout>
+          }
+        />
+      </Route>
       <Route element={<ProtectedRoute />}>
         <Route
           path="/user-profile"
--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,9 +1,14 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useGetMyRestaurantOrders,
   useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
+import OrderItemCard from "@/components/OrderItemCard";
+
+import { Tabs, TabsList } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+import { TabsContent, TabsTrigger } from "@radix-ui/react-tabs";
 
 const ManageRestaurantPage = () => {
   const { createRestaurant, isLoading: isCreateLoading } =
@@ -11,15 +16,34 @@ const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: isUpdateLoading } =
     useUpdateMyRestaurant();
+  const { orders } = useGetMyRestaurantOrders();
 
   const isEditing = !!restaurant; //This is a double negation trick to convert any value into a boolean, if restaurant exist then return true else return false
 
   return (
-    <ManageRestaurantForm
-      isLoading={isCreateLoading || isUpdateLoading}
-      restaurant={restaurant}
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-    />
+    <Tabs defaultValue="orders">
+      <TabsList className="flex gap-3">
+        <TabsTrigger value="orders">Orders</TabsTrigger>
+        <TabsTrigger value="manage-restaurant">ManageRestaurant</TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => (
+          // ?because orders could be an empty list
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
