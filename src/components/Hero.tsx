ac77f5d4fdf89b0456ceb85146e2d0256479ab0c
Ritik <ritikraj198@gmail.com>
2025-09-28 04:18:36 +0530

    doing some layout changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import hero from "../assets/hero.png";
+import hero from "../assets/food3.jpg";
 
 const Hero = () => {
   return (
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import landingImage from "../assets/landing.png";
-import appDownloadImage from "../assets/appDownload.png";
+// import landingImage from "../assets/landing2.png";
+// import appDownloadImage from "../assets/appDownload.png";
 import SearchBar, { type SearchForm } from "@/components/SearchBar";
 import { useNavigate } from "react-router-dom";
 
@@ -17,9 +18,9 @@ const HomePage = () => {
     <div className="flex flex-col gap-12">
       <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-          Tuck into a takeaway today
+          Your meal, on the move.
         </h1>
-        <span className="text-xl">Food is just a click away</span>
+        <span className="text-xl">Fast, fresh, wherever you are</span>
         <SearchBar
           placeHolder="Search by city or Town"
           onSubmit={handleSearchSubmit}
@@ -27,17 +28,18 @@ const HomePage = () => {
       </div>
 
       {/* When the screen size is medium (≥768px) or larger, use 2 columns in the grid. OTHERWISE DEFAULT TO grid which by def has col-1 */}
-      <div className="grid md:grid-cols-2 gap-5">
-        <img src={landingImage} />
-        <div className="flex flex-col items-center justify-center gap-4 text-center">
+      <div className="grid md:grid-cols-2 gap-8 items-center">
+        <img
+          src={landingImage}
+          width={300}
+          height={300}
+          className="mx-auto md:ml-auto md:mr-0"
+        />
+
+        <div className="flex flex-col items-center justify-center gap-4 text-center max-w-md mx-auto">
           <span className="font-bold text-3xl tracking-tighter">
-            Order takeaway even faster!
+            From app to appetite in no time!
           </span>
-          <span>
-            Download the MernEats App for faster ordering and personalised
-            recommendations
-          </span>
-          <img src={appDownloadImage} />
         </div>
       </div>
     </div>
